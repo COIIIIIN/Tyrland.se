611d02d81eb7c9b4519d4414e9f9c720f9e72cc1
Coiiiiin <alvin.tyrland@gmail.com>
2025-07-29 15:25:51 +0200

    Changed small images to base64 for fewer requests

--- www.Tyrland.se/Faults/404-not_found.php
+++ www.Tyrland.se/Faults/404-not_found.php
@@ -4,7 +4,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="/style.php">
-	<link rel="icon" href="/Res/icon.png">
+	<link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAA
+	AAXNSR0IArs4c6QAAAVBJREFUeJztmrENwkAMRRNEwRQwARNRIlqGoUWUTMQEMAVdqONIsSxf8pD8X5nA5efLdmLnuk4
+	IUZneHjjfh4EQshaPSz+65w0l5F+QAbQAGhlAC6CRAbQAmq098H19Fr3g7rifPb/09S3lI0AG0AJoJr1AFttL2Hdvr9f
+	wfm/PZykfATKAFkAzeQ+Isvb8oHVNKB8BMoAWQBOuAV7Ot36Xt+vZXiJbE8pHgAygBdC4+RLN+eftMLtmtBewnK7v0f+
+	9+YK3XvkIkAG0AJrwt8FozkfXjz7HszWhfATIAFoATbgXiOb80lg90flE+QiQAbQAGvfbYOv+uzXZ+UT5CJABtAAaN39
+	b99/ZXqB1r1I+AmQALYAm/AzP1oTs/gBLdj5RPgJkAC2AJj0PiNaELNmct5SPABlAC6BJ7xHyaoIlun+gdc5bykeADKA
+	FCCEEyQ8lpYsmnaLllQAAAABJRU5ErkJggg==">
 	<title>404 - Not Found</title>
 </head>
 <body>
